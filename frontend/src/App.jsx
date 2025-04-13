a01fb2341e53881947581726a0aabf59b33e0899
dmfshove <funnymoments610@gmail.com>
2025-04-13 16:50:23 +0200

    feat(pages): create page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import FormPage from "./pages/FormPage/FormPage"
+import AccountPage from "./pages/AccountPage/AccountPage"
+import ErrorPage from "./pages/ErrorPage/ErrorPage"
 
 export default function App() {
     return (
-        <h1>Hello World!</h1>
+        <Router>
+            <Routes>
+                <Route path="/" element={<FormPage />} />
+                <Route path="/account" element={<AccountPage />} />
+                <Route path="*" element={<ErrorPage />} />
+            </Routes>
+        </Router>
     )
 }

c5c7dce9f375149cbe089c9cd2e908759b771b4d
dmfshove <funnymoments610@gmail.com>
2025-04-21 17:36:12 +0200

    fix(backend): fix import in database.py and models.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from database import SessionLocal, engine
-import models, schemas
+from app.database import SessionLocal, engine
+import app.models, app.schemas
 from passlib.context import CryptContext
 from typing import List
 

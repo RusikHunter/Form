3bf755375d04d6380a99f41ddaa89c49dbc2aaf0
dmfshove <funnymoments610@gmail.com>
2025-07-17 21:48:08 +0200

    feat(account): save account authorization between sessions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,29 @@ import FormPage from "./pages/FormPage/FormPage"
 import AccountPage from "./pages/AccountPage/AccountPage"
 import ErrorPage from "./pages/ErrorPage/ErrorPage"
 import Header from "./components/Header/Header"
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
+import { setIsLogged, setCurrentUser } from "./store/reducers/userReducer"
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute"
 
 export default function App() {
     const theme = useSelector(state => state.client.theme)
     const isLogged = useSelector(state => state.user.isLogged)
+    const currentUser = useSelector(state => state.user.currentUser)
+
+    const dispatch = useDispatch()
 
     useEffect(() => {
         document.body.setAttribute("data-js-theme", theme)
     }, [theme])
+
+    useEffect(() => {
+        localStorage.setItem("isLogged", JSON.stringify(isLogged))
+    }, [isLogged])
+
+    useEffect(() => {
+        localStorage.setItem("currentUser", JSON.stringify(currentUser))
+    }, [currentUser])
 
     return (
         <div data-testid="app-component">
@@ -21,12 +34,16 @@ export default function App() {
             <main className="main">
                 <Routes>
                     <Route path="/" element={<FormPage />} />
-                    {isLogged
-                        ?
-                        <Route path="/account" element={<AccountPage />} />
-                        :
-                        <Route path="/account" element={<FormPage />} />
-                    }
+
+                    <Route
+                        path="/account"
+                        element={
+                            <PrivateRoute isLogged={isLogged}>
+                                <AccountPage />
+                            </PrivateRoute>
+                        }
+                    />
+
                     <Route path="*" element={<ErrorPage />} />
                 </Routes>
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,19 @@ import { configureStore } from '@reduxjs/toolkit'
 import clientReducer from "./reducers/clientReducer"
 import userReducer from './reducers/userReducer'
 
+const preloadedState = {
+    user: {
+        isLogged: JSON.parse(localStorage.getItem("isLogged")) || false,
+        currentUser: JSON.parse(localStorage.getItem("currentUser")) || {}
+    }
+}
+
 const store = configureStore({
     reducer: {
         client: clientReducer,
         user: userReducer,
     },
+    preloadedState
 })
 
 export default store
